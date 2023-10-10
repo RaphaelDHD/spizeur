4d360e5671cc753e70227bf0647b8aaa21c9801b
DECHENAUD RAPHAEL p2106803 <raphael.dechenaud@etu.univ-lyon1.fr>
2023-10-10 23:31:54 +0200

    v0.1 : Display of all the products with product_fragment.xml

--- app/src/main/java/com/example/spizeur/ui/MainActivity.kt
+++ app/src/main/java/com/example/spizeur/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.spizeur.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.HorizontalScrollView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
@@ -12,6 +15,7 @@ import com.example.spizeur.databinding.ActivityMainBinding
 import com.example.spizeur.models.Product
 import com.example.spizeur.models.services.ApiClient
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -46,6 +50,60 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+    private fun sortProductsByCategory(products: List<Product>): MutableMap<String, MutableList<Product>> {
+        val productsByCategory = mutableMapOf<String, MutableList<Product>>()
+
+        for (product in products) {
+            if (!productsByCategory.containsKey(product.category)) {
+                productsByCategory[product.category] = mutableListOf()
+            }
+            productsByCategory[product.category]?.add(product)
+        }
+        return productsByCategory
+    }
+
+    private fun showByCategory(productsByCategory: MutableMap<String, MutableList<Product>>) {
+        val container = findViewById<LinearLayout>(R.id.Categories) // Référence à votre LinearLayout dans le XML
+
+        runOnUiThread(Runnable {
+            for (productCategory in productsByCategory) {
+                val categoryTitle = TextView(this@MainActivity)
+                categoryTitle.text = productCategory.key
+                container.addView(categoryTitle)
+
+                val horizontalScrollView = HorizontalScrollView(this@MainActivity)
+                container.addView(horizontalScrollView)
+
+                val linearLayout = LinearLayout(this@MainActivity)
+                linearLayout.orientation = LinearLayout.HORIZONTAL
+                horizontalScrollView.addView(linearLayout)
+
+                for ((index, product) in productCategory.value.withIndex()) {
+                    val fragmentView = layoutInflater.inflate(R.layout.product_fragment, null) // Remplacez "your_fragment_layout" par le nom de votre fragment XML
+                    val titleTextView = fragmentView.findViewById<TextView>(R.id.Title)
+                    val priceTextView = fragmentView.findViewById<TextView>(R.id.Price)
+
+                    titleTextView.text = product.title
+                    priceTextView.text = product.price.toString() + "€"
+
+                    // Ajoutez une marge à droite de 16 pixels, sauf pour le dernier élément
+                    val layoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                    if (index < productCategory.value.size - 1) {
+                        layoutParams.setMargins(0, 0, 16, 0)
+                    }
+                    fragmentView.layoutParams = layoutParams
+
+                    linearLayout.addView(fragmentView)
+                }
+            }
+        })
+    }
+
+
+    @OptIn(DelicateCoroutinesApi::class)
     private fun executeCall() {
         GlobalScope.launch(Dispatchers.IO) {
             try {
@@ -53,20 +111,12 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful && response.body() != null) {
                     val content = response.body()
                     val products = content?.products
+                    val productsByCategory = sortProductsByCategory(products!!)
 
-                    val productsByCategory = mutableMapOf<String, MutableList<Product>>()
-
-                    for (product in products!!) {
-                        if (!productsByCategory.containsKey(product.category)) {
-                            productsByCategory[product.category] = mutableListOf()
-                        }
-                        productsByCategory[product.category]?.add(product)
-                    }
-                    for (product in productsByCategory) {
-                        Log.e("API_TEST", "${product.key} : ${product.value}")
-                    }
+                    showByCategory(productsByCategory)
+                    Log.d("API_TEST", "Reponse correcte")
                 } else {
-                    Log.e("API_TEST", "Reponse pas correcte")
+                    Log.d("API_TEST", "Reponse pas correcte")
                 }
 
             } catch (e: Exception) {
@@ -75,6 +125,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    private fun Int.dpToPx(): Int {
+        val density = resources.displayMetrics.density
+        return (this * density).toInt()
+    }
 
 }
