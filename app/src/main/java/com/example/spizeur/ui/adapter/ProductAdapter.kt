d6ca3ef5c690a9c172d98965c506dedecd486a9e
DECHENAUD RAPHAEL p2106803 <raphael.dechenaud@etu.univ-lyon1.fr>
2023-11-30 18:52:27 +0100

    objets produits maquette

--- app/src/main/java/com/example/spizeur/ui/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/spizeur/ui/adapter/ProductAdapter.kt
@@ -1,18 +1,18 @@
 package com.example.spizeur.ui.adapter
 
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat.startActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.spizeur.R
 import com.example.spizeur.domain.ProductsRepository
 import com.example.spizeur.models.Product
-import com.example.spizeur.ui.home.HomeViewModel
 import com.example.spizeur.ui.productInfo.ProductInfoActivity
+import com.squareup.picasso.Picasso
 
 class ProductAdapter (private var productData: List<Product>):
     RecyclerView.Adapter<ProductAdapter.DataViewHolder>()
@@ -37,8 +37,11 @@ class ProductAdapter (private var productData: List<Product>):
         }
 
         fun bind(result : Product) {
-            itemView.findViewById<TextView>(R.id.name).text = result.title
-
+            itemView.findViewById<TextView>(R.id.productTitle).text = result.title
+            itemView.findViewById<TextView>(R.id.productPrice).text = result.price.toString() + '€'
+            val imageUrl = result.thumbnail
+            val image = itemView.findViewById<ImageView>(R.id.productImage)
+            Picasso.get().load(imageUrl).resize(160, 73).into(image)
         }
 
     }
