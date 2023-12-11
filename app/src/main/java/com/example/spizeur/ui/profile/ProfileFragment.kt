7d64ce4eed5a47b3c22e95146f4a89863fdd5e4b
Rakel TAFA <rakeltafa.pro@gmail.com>
2023-12-11 19:29:07 +0100

    Correction retour en arrière de Settings à ProfileFragment

--- app/src/main/java/com/example/spizeur/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/spizeur/ui/profile/ProfileFragment.kt
@@ -7,21 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
-import com.example.spizeur.ProfileViewModel
 import com.example.spizeur.R
 import com.example.spizeur.databinding.FragmentProfileBinding
-import com.example.spizeur.ui.home.HomeActiviy
 import com.example.spizeur.ui.settings.SettingsActivity
-import com.google.android.material.button.MaterialButton
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,7 +36,6 @@ class ProfileFragment : Fragment() {
         view.findViewById<LinearLayout>(R.id.profile_settings_container)?.setOnClickListener {
             val intent = Intent(this.context, SettingsActivity::class.java)
             startActivity(intent)
-            requireActivity().finish()
         }
     }
 }
--- app/src/main/java/com/example/spizeur/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/spizeur/ui/settings/SettingsActivity.kt
@@ -3,10 +3,7 @@ package com.example.spizeur.ui.settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import androidx.fragment.app.FragmentManager
-import androidx.navigation.findNavController
 import com.example.spizeur.R
-import com.google.android.material.button.MaterialButton
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +11,7 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         findViewById<ImageView>(R.id.settings_return_arrow)?.setOnClickListener {
-            //it.findNavController().popBackStack(R.id.nav_home_fragment, true)
-            // TODO Faire retour en arriere de Settings à Home/profile.
+            finish()
         }
     }
 
