92414d3981c952b05295d77f2bfdc50845ea7092
IUTABGDIN105-01\Administrateur <iutbourg.smir@univ-lyon1.fr>
2024-02-16 22:13:15 +0100

    test firebase auth (pas fonctionnel à continuer)

--- app/src/main/java/com/example/spizeur/SpizeurApp.kt
+++ app/src/main/java/com/example/spizeur/SpizeurApp.kt
@@ -2,9 +2,12 @@ package com.example.spizeur
 
 import android.app.Application
 import com.example.spizeur.domain.database.SpizeurDataBase
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import timber.log.Timber
 
 class SpizeurApp: Application() {
+
+    private lateinit var signInRequest: BeginSignInRequest
 
     override fun onCreate() {
         super.onCreate()
@@ -12,6 +15,17 @@ class SpizeurApp: Application() {
         Timber.plant(Timber.DebugTree())
 
         SpizeurDataBase.initDatabase(context = applicationContext)
+
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                // Your server's client ID, not your Android client ID.
+                .setServerClientId(getString(R.string.default_web_client_id))
+                // Only show accounts previously used to sign in.
+                .setFilterByAuthorizedAccounts(true)
+                .build())
+            .build();
     }
 
 }
--- app/src/main/java/com/example/spizeur/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/spizeur/ui/login/LoginFragment.kt
@@ -2,28 +2,54 @@ package com.example.spizeur.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.spizeur.R
 import com.example.spizeur.databinding.FragmentLoginBinding
 import com.example.spizeur.MainActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginFragment : Fragment() {
+
+
+    private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient : GoogleSignInClient
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     private lateinit var vm: LoginViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vm = ViewModelProvider(this)[LoginViewModel::class.java]
-    }
 
+        val currentUser: FirebaseUser? = mAuth.currentUser
+
+        val googleSignIn = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this.requireActivity(), googleSignIn)
+
+        view?.findViewById<com.google.android.gms.common.SignInButton>(R.id.google_login_button)?.setOnClickListener {
+
+        }
+
+        if (currentUser != null) updateUI(currentUser)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -70,7 +96,23 @@ class LoginFragment : Fragment() {
         vm.login(email, password, requireContext())
     }
 
+    private fun updateUI(firebaseUser : FirebaseUser?)
+    {
+        Log.d("USER_FIREBASE", firebaseUser.toString())
+    }
 
+
+    // https://www.youtube.com/watch?v=hcEqX5lcuUc
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    {
+
+    }
+
+    private fun googleSignIn()
+    {
+        val signInClient = googleSignInClient.signInIntent
+
+    }
 
 
 }
